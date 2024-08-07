86fc4add15a906cd4a04fc3e0edd0926aa3df357
apollosuny <baotrung06092003@gmail.com>
2024-08-07 22:55:23 +0700

    change mimetype

--- src/lib/constants/post.ts
+++ src/lib/constants/post.ts
@@ -14,7 +14,7 @@ export const maximumUploadedImages = 10;
 export const imageLimitSize = 5 * 1024 * 1024; // 5MB
 export const videoLimitSize = 100 * 1024 * 1024; // 100MB
 
-export const mimeType = 'video/webm';
+export const mimeType = 'video/mp4';
 /**
  * Minus 1 second to get the correct duration
  * Example: expect duration is 15s --> maxDuration is 14s
